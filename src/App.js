d063fe8839fb2812b4bbea1a81ff74379ea03ee7
Yash Tulsani <yash.t20@iiits.in>
2023-12-15 02:08:44 +0530

    added 360 degree on home page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
-
 import Contact from './components/Contact/Contact';
 import Status from './components/Status/Status';
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SearchPanel from '../../components/SearchPanel/SearchPanel'
+import Image360 from '../../components/Image360/Image360'
 
 const Home = () => {
   return (
     <>
         <SearchPanel/>
+        <Image360/>
     </>
   )
 }
