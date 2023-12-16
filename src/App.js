576ae4b0d524dfda8e497dc97ffdbe3c8214cfa2
Tejas <tejasparse1@gmail.com>
2023-12-16 19:51:22 +0530

    Added Chat Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Contact from './components/Contact/Contact';
 import Status from './components/Status/Status';
+import Chatbot from './components/Chatbot/Chatbot';
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <Route path="/contact" element={<Contact/>} />
         <Route path="/status" element={<Status/>} />
       </Routes>
+      <Chatbot/>
     </Router>
   );
 }
--- src/components/Status/Status.jsx
+++ src/components/Status/Status.jsx
@@ -5,6 +5,9 @@ import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+
 
 const Status = () => {
     
@@ -29,6 +32,18 @@ const Status = () => {
 
         setLoading(true);
 
+        // axios.get(`${process.env.REACT_APP_API_URL}/status`,{
+        //     applicationType,
+        //     applicationId
+        // })
+        // .then(res=>{
+        //     let response = res.data;
+        //     console.log(response);
+        // })
+        // .catch(err=>{
+        //     console.log(err, "This is the error");
+        // })
+        
         setTimeout(() => {
             setLoading(false);
 
@@ -38,6 +53,12 @@ const Status = () => {
 
         }, 2000);
         
+    }
+
+    const resetForm = (event) => {
+        setResult(null);
+        setApplicationId();
+        setApplicationType('')
     }
 
     return (
@@ -50,47 +71,50 @@ const Status = () => {
                 (
                     result ? (
                         <div className='mt-10 text-xl'>
-                            {result.status}
+                            <div>
+                                {result.status}
+                            </div>
+                            <Button onClick={resetForm} style={{color:"white"}} variant="contained" color='secondary'>Reset</Button>
                         </div>
                     ) : (<div class="mt-10 sm:mx-auto sm:w-full sm:max-w-xl rounded border-2 border-black p-5">
-                    <form class="space-y-6" onSubmit={handleSubmit} >
-                        <div>
+                            <form class="space-y-6" onSubmit={handleSubmit} >
+                                <div>
+                                
+                                    <label for="email" class="block text-md font-medium leading-6 text-gray-900 mb-2">Type of Application</label>
+                                    <FormControl sx={{ width: "100%" }}>
+                                        <Select
+                                            value={applicationType}
+                                            onChange={handleChangeAppType}
+                                            required
+                                            inputProps={{ 'aria-label': 'Without label' }}
+                                            sx={{ border: '1px solid #F58F3C', borderRadius: '7px' }}
+                                        >
+                                    
+                                            <MenuItem value={"Service"}>Service Application</MenuItem>
+                                            <MenuItem value={"WDC"}>WDC Application</MenuItem>
+                            
+                                        </Select>
+                            
+                                    </FormControl>
+                                </div>
+
+                                <div>
+                                    <div class="flex items-center justify-between">
+                                        <label for="password" class="block text-md font-medium leading-6 text-gray-900">Application Id</label>
                         
-                            <label for="email" class="block text-md font-medium leading-6 text-gray-900 mb-2">Type of Application</label>
-                            <FormControl sx={{ width: "100%" }}>
-                                <Select
-                                    value={applicationType}
-                                    onChange={handleChangeAppType}
-                                    required
-                                    inputProps={{ 'aria-label': 'Without label' }}
-                                    sx={{ border: '1px solid #F58F3C', borderRadius: '7px' }}
-                                >
-                               
-                                    <MenuItem value={"Service"}>Service Application</MenuItem>
-                                    <MenuItem value={"WDC"}>WDC Application</MenuItem>
-                    
-                                </Select>
-                    
-                            </FormControl>
-                        </div>
+                                    </div>
+                                    <div class="mt-2">
+                                        <input id="application_id" value={applicationId} onChange={handleChangeAppId} name="application_id" type="number" required class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-[#F58F3C] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#F58F3C] sm:text-lg sm:leading-6"/>
+                                    </div>
+                                </div>
 
-                        <div>
-                            <div class="flex items-center justify-between">
-                                <label for="password" class="block text-md font-medium leading-6 text-gray-900">Application Id</label>
-                
-                            </div>
-                            <div class="mt-2">
-                                <input id="application_id" value={applicationId} onChange={handleChangeAppId} name="application_id" type="number" required class="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-[#F58F3C] placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#F58F3C] sm:text-lg sm:leading-6"/>
-                            </div>
-                        </div>
+                                <div>
+                                    <button type="submit" class="flex w-full justify-center shadow-xl rounded-md bg-[#F58F3C] px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-[rgb(207,112,35)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[rgb(255,170,101)]">Submit</button>
+                                </div>
+                            </form>
 
-                        <div>
-                            <button type="submit" class="flex w-full justify-center shadow-xl rounded-md bg-[#F58F3C] px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-[rgb(255,170,101)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[rgb(255,170,101)]">Submit</button>
-                        </div>
-                    </form>
-
-                 
-                </div>)
+                        
+                        </div>)
 
                     
                 ) }
