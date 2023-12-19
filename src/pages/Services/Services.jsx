7c65b3dc0279c1e9a541cb8e0fae8a06bb99da09
GoViper <nithishbanda2021@gmail.com>
2023-12-19 09:46:28 +0530

    services-BackendConnect

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -4,18 +4,36 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import theme from '../../theme'; 
 import L from 'leaflet';
-
-
-const services = [
-  { name: 'Service 1', category: 'Category A', location: [51.505, -0.09], image: "https://htmlcolorcodes.com/assets/images/colors/bright-orange-color-solid-background-1920x1080.png" },
-  { name: 'Service 2', category: 'Category B', location: [51.515, -0.1], image: "https://htmlcolorcodes.com/assets/images/colors/bright-orange-color-solid-background-1920x1080.png" },
-  { name: 'Service 3', category: 'Category A', location: [51.535, -0.1], image: "https://htmlcolorcodes.com/assets/images/colors/bright-orange-color-solid-background-1920x1080.png" },
-  { name: 'Service 4', category: 'Category B', location: [51.555, -0.1], image: "https://htmlcolorcodes.com/assets/images/colors/bright-orange-color-solid-background-1920x1080.png" },
-  { name: 'Service 5', category: 'Category A', location: [51.575, -0.1], image: "https://htmlcolorcodes.com/assets/images/colors/bright-orange-color-solid-background-1920x1080.png" },
-  // Add more services as needed
-];
+import axios from 'axios';
 
 const Services = () => {
+  const buyFunc = (id) => {
+    const data = axios.get(`http://localhost:5000/api/service/${id}`)
+        .then((response) => {
+          console.log(response.data)
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    }
+  const [services, setServices] = React.useState([]);
+  React.useEffect(() => {
+    const populateServices = () => {
+      const data = axios.get('http://localhost:5000/api/service/')
+        .then((response) => {
+          console.log(response.data)
+          for (let i = 0; i < response.data.services.length; i++) {
+            response.data.services[i].location = [13.0827 + (Math.random() * 1.8) - 0.9, 79.2707 + (Math.random() * 1.8) - 0.9];
+          }
+          setServices(response.data.services)
+          console.log(services)
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }
+    populateServices();
+  }, [])
   const [selectedService, setSelectedService] = React.useState(null);
   const [selectedCategory, setSelectedCategory] = React.useState('All'); // Initial state for the category filter
   const mapRef = React.useRef(null);
@@ -71,26 +89,26 @@ const Services = () => {
                 placeHolder="Filter by Category">
                 <option value="All">Filter by Category</option>
                 {/* Add more options for each unique category */}
-                {Array.from(new Set(services.map(service => service.category))).map((category, index) => (
+                {services.length !=0 ? Array.from(new Set(services.map(service => service.category))).map((category, index) => (
                   <option key={index} value={category}>{category}</option>
-                ))}
+                )) : null}
               </select>
             </Grid>
           </Grid>
-          <Grid container spacing={2}>
+          <Grid container spacing={2} style={{marginTop: '20px'}}>
             {/* First column with cards */}
             <Grid item xs={6} style={{ height: 'calc(100vh - 40px)', overflowY: 'auto' }}>
-              {filteredServices.map((service, index) => (
+              {filteredServices.length != 0 ? filteredServices?.map((service, index) => (
                 <Card key={index} variant="outlined" style={{ marginBottom: '20px', boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)' }}>
                   {/* Card image */}
-                  <img src={service.image} alt={service.name} style={{ width: '100%', height: '60px', borderTopLeftRadius: '4px', borderTopRightRadius: '4px' }} />
+                  <img src={service.images.length == 0 ? "https://htmlcolorcodes.com/assets/images/colors/orange-color-solid-background-1920x1080.png" : service.images[0]} alt={service.name} style={{ width: '100%', height: '60px', borderTopLeftRadius: '4px', borderTopRightRadius: '4px' }} />
 
                   <CardContent>
                     {/* Card content */}
                     <Typography variant="h6">{service.name}</Typography>
                     <Typography variant="subtitle1" color="textSecondary">{service.category}</Typography>
                     <Typography variant="body2" color="textSecondary" style={{ marginTop: '10px' }}>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      {service.description}
                     </Typography>
 
                     {/* Buttons */}
@@ -98,6 +116,7 @@ const Services = () => {
                       variant="contained"
                       color="primary"
                       style={{ marginTop: '10px', marginRight: '10px' }}
+                      onClick={() => buyFunc(service._id)}
                     >
                       Buy Now
                     </Button>
@@ -111,14 +130,14 @@ const Services = () => {
                     </Button>
                   </CardContent>
                 </Card>
-              ))}
+              )) : null}
             </Grid>
 
             {/* Second column with Leaflet map */}
             <Grid item xs={6}>
               <MapContainer
                 ref={mapRef}
-                center={selectedService ? selectedService.location : [51.505, -0.09]} // Center of the map
+                center={selectedService ? selectedService.location : [13.0827, 79.2707]} // Center of the map
                 zoom={13} // Initial zoom level
                 style={{ height: '100%', width: '140%' }}
               >
@@ -126,7 +145,7 @@ const Services = () => {
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                   attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 />
-                {services.map((service, index) => (
+                {services.length != 0 ? services.map((service, index) => (
                   <Marker key={index} position={service.location} icon={selectedService === service ? selectedMarkerIcon : defaultMarkerIcon} >
                     {selectedService === service && (
                       <Popup onClose={() => setSelectedService(null)}>
@@ -136,7 +155,7 @@ const Services = () => {
                       </Popup>
                     )}
                   </Marker>
-                ))}
+                )) : null}
               </MapContainer>
             </Grid>
           </Grid>
