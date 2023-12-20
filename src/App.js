a9c5597ff8279b1e3809ee0ae6b72fc4e9591c60
Tejas <tejasparse1@gmail.com>
2023-12-20 17:03:24 +0530

    new charts

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import TestGraph from './components/TestGraph';
 import Dashboard from './pages/Dashboard/Dashboard';
 import TextToSpeech from './components/TestTextToSpeech';
 import AvailableWDCs from './pages/AvailableWDCs/AvailableWDCs';
+import OverallAnalysis from './components/DashboardComponents/OverallAnalysis/OverallAnalysis';
 
 function App() {
 
@@ -38,6 +39,7 @@ function App() {
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/profile" element={<Profile/>} /> 
         <Route path="/wdcs" element={<AvailableWDCs/>} />       
+        {/* <Route path="/analysis" element={<OverallAnalysis/>} />        */}
         <Route path="/dashboard" element={<Dashboard/>}>
           <Route path="statistics" element={"Statistics"}/>
           <Route path="transactions" element={"Transactions"}/>
